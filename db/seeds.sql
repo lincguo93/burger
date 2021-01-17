9c89e0d0b7d46b96ec0c2af733d3c2fcfd00740e
Daoyuan Guo <linkingarden@foxmail.com>
2021-01-17 15:49:00 -0800

    updated database

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Double Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Triple Patties Burger');
+INSERT INTO burgers (burger_name) VALUES ('Mt. Fuji Burger');
