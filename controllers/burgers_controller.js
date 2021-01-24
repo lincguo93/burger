8da4dd3c7b06c01e785d4ff8ccb65861d0ab6ba3
Daoyuan Guo <linkingarden@foxmail.com>
2021-01-24 15:31:44 -0800

    completing the rest

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,59 @@
+var express = require("express");
+
+var router = express.Router();
+
+var burgers = require("../models/burger");
+
+router.get("/", function(req, res) {
+  burgers.all(function(data) {
+    var burgerObject = {
+      burgers: data
+    };
+    console.log(burgerObject);
+    res.render("index", burgerObject);
+  });
+});
+
+router.post("/api/burgers", function(req, res) {
+  burgers.insertOne([
+    "burger_name", "devoured"
+  ], [
+    req.body.name, req.body.devoured
+  ], function(result) {
+    // Send back the ID of the new quote
+    res.json({ id: result.insertId });
+  });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("id", condition);
+
+  burgers.updateOne({
+    devoured: req.body.devoured
+  }, condition, function(result) {
+    if (result.changedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+router.delete("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  cat.delete(condition, function(result) {
+    if (result.affectedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+// Export routes for server.js to use.
+module.exports = router;
